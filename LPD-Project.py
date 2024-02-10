ea40befd92df0006fbd0149b66a946ac81152e2a
Vinicius Rodrigues <viniciusrodrigues_5@hotmail.com>
2024-02-10 19:32:23 +0000

    Melhoria visualização do código

--- LPD-Project.py
+++ LPD-Project.py
@@ -5,7 +5,6 @@ import random
 import time
 import string
 import subprocess
-# from scapy.all import *
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from cryptography.hazmat.backends import default_backend
@@ -48,12 +47,10 @@ def flood_udp(ip_alvo):
         sock.sendto(bytes_to_send, (ip_alvo, random.randint(1, 65535)))
         sent += 1
         print(f"Sent {sent} amount of packets to {ip_alvo}")
-
-        # ------------->   Caso queira limitar o número de Pacotes   <--------------
+        # Caso queira limitar o número de Pacotes --->
         # if sent >= 100000:
-            # print("Parando UDP Flood após 1000 pacotes.")
             # break
-        # ------------->   Caso queira limitar o número de Pacotes   <--------------
+        # Caso queira limitar o número de Pacotes <---
 
 
 # Based on "How to Make a SYN Flooding Attack in Python" / "Abdeladim Fadheli" Article - https://thepythoncode.com/article/syn-flooding-attack-using-scapy-in-python --->
@@ -76,7 +73,7 @@ def generate_key_from_password(password, salt):
         iterations=100000,
         backend=default_backend()
     )
-    key = kdf.derive(password.encode())  # Deriva uma chave a partir da senha
+    key = kdf.derive(password.encode()) 
     return key
 
 def encrypt_message(key, message, salt):
@@ -99,7 +96,7 @@ def server_side(port, password):
         s.bind(('127.0.0.1', port))
         s.listen()
         conn, addr = s.accept()
-        conn.send("Thank you for connecting".encode())
+        conn.send("Conectado".encode())
         dataFromClient = conn.recv(4096)
         decrypted_data = decrypt_message(password, dataFromClient)
         print(decrypted_data.decode())
@@ -109,13 +106,13 @@ def server_side(port, password):
 
 def client_side(ip, port, password):
     try:
-        salt = os.urandom(16)  # Gera um salt único para esta sessão
+        salt = os.urandom(16) 
         key = generate_key_from_password(password, salt)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((ip, port))
         dataFromServer = s.recv(1024)
         print(dataFromServer.decode())
-        dataToServer = "Hello Server From Client"
+        dataToServer = "Conectando"
         encrypted_data = encrypt_message(key, dataToServer.encode(), salt)
         s.send(encrypted_data)
         s.close()
@@ -130,14 +127,12 @@ def port_knocking():
     for port in knocking_ports:
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
             s.sendto(b'', (target_ip, port))
-            # Define o tempo de comunicação entre as portas
-            time.sleep(1)
+            time.sleep(1) # Define o tempo de comunicação entre as portas
 
 
 def Menu_do_Programa():
     os.system('clear')
-    print("\nLinguagens de Programação Dinâmicas")
-    print("Vinícius Rodrigues\n")
+    print("\nLPD-Project | Linguagens de Programação Dinâmicas")
     print("1- Port Scan")
     print("2- UDP Flood")
     print("3- SYN Flood")
@@ -153,29 +148,33 @@ def main():
         Menu_do_Programa()
         escolha = input("Escolha uma opção: ")
         os.system('clear')
+
         if escolha == "1":
             print("1- Port Scan\n")
             faixa_ips_alvo = input("Digite o IP(s) alvo (Ex. 192.168.0.1-25): ")
             porta_inicial, porta_final = map(int, input("Digite o intervalo de portas (Ex 1-1000): ").split('-'))
             varredura_de_portas(faixa_ips_alvo, porta_inicial, porta_final)
             input("Pressione Enter para continuar...")
+
         elif escolha == "2":
             print("2- UDP Flood\n")
             ip_alvo = input("Digite o IP alvo: ")
             flood_udp(ip_alvo)
+
         elif escolha == "3":
             print("3- SYN Flood\n")
             ip_alvo = input("Digite o IP alvo: ")
             porta_alvo = int(input("Digite a porta alvo: "))
             syn_flood(ip_alvo, porta_alvo)
             input("Pressione Enter para continuar...")
+
         elif escolha == "4":
-            print("Você escolheu a Opção 4")
-            nome_arquivo = input("Digite o nome do arquivo de log: ")
-            contar_ocorrencias_ips(nome_arquivo)
+            print("4- Análise e Processamento de Ficheiros de Log\n")
+            print("Este módulo está em construção. Atualize o código e volte mais tarde, obrigado!\n\n")
             input("Pressione Enter para continuar...")
+
         elif escolha == "5":
-            print("Você escolheu a Opção 5")
+            print("5- Troca de Mensagens\n")
             escolha_server_client = input("Digite 1 para servidor ou 2 para cliente: ")
             if escolha_server_client == '1':
                 port = int(input("Digite a porta do servidor: "))
@@ -189,8 +188,9 @@ def main():
             else:
                 print("Opção inválida.")
                 input("Pressione Enter para continuar...")
+
         elif escolha == "6":
-            print("6- Port Knocking")
+            print("6- Client Port Knocking\n")
             print("Para configurar um servidor, primeiro utilize seguinte script: \n\n* filter\n:INPUT DROP [0:0]\n:FORWARD DROP [0:0]\n:OUTPUT ACCEPT [0:0]\n:TRAFFIC - [0:0]\n:SSH-INPUT - [0:0]\n:SSH-INPUTTWO - [0:0]\n# TRAFFIC chain for Port Knocking. The correct port sequence in this example is 8881 -> 7777 -> 9991; any other sequence will drop the traffic\n-A INPUT -j TRAFFIC\n-A TRAFFIC -p icmp --icmp-type any -j ACCEPT\n-A TRAFFIC -m state --state ESTABLISHED, RELATED -j ACCEPT\n-A TRAFFIC -m state --state NEW -m tcp -p tcp --dport 22 -m recent --rcheck --seconds 30 --name SSH2 -j ACCEPT\n-A TRAFFIC -m state --state NEW -m tcp -p tcp -m recent --name SSH2 --remove -j DROP\n-A TRAFFIC -m state --state NEW -m tcp -p tcp --dport 9991 -m recent --rcheck --name SSH1 -j SSH-INPUTTWO\n-A TRAFFIC -m state --state NEW -m tcp -p tcp -m recent --name SSH1 --remove -j DROP\n-A TRAFFIC -m state --state NEW -m tcp -p tcp --dport 7777 -m recent --rcheck --name SSHO -j SSH-INPUT\n-A TRAFFIC -m state --state NEW -m tcp -p tcp -m recent --name SSHO --remove -j DROP\n-A TRAFFIC -m state --state NEW -m tcp -p tcp --dport 8881 -m recent --name SSHO --set -j DROP\n-A SSH-INPUT -m recent --name SSH1 --set -j DROP\n-A SSH-INPUTTWO -m recent --name SSH2 --set -j DROP\n-A TRAFFIC -j DROP\nCOMMIT\n# END or further rules")
             input("Pressione Enter para continuar...")
             os.system('clear')
@@ -199,11 +199,13 @@ def main():
             os.system('clear')
 
         elif escolha == "7":
-            print("Você escolheu a Opção 7")
+            print("7- Reverse Shell\n")
             input("Pressione Enter para continuar...")
+
         elif escolha == "0":
-            print("Saindo do programa")
+            print("Saindo do programa...")
             break
+
         else:
             print("Opção Inválida")
             input("Pressione Enter para continuar...")
